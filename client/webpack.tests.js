50df4e89698f5421c159aa0671a2e5a1b13b64f7
Matthew Findlater <matthew.findlater@gmail.com>
2022-04-14 15:59:45 -0400

    client test wip

--- client/webpack.tests.js
+++ client/webpack.tests.js
@@ -19,27 +19,11 @@ var CONFIG = {
     indexHtmlTemplate: "./tests/index.html",
     fsharpEntry: "./tests/Tests.fs.js",
     outputDir: "./dist",
-    assetsDir: "./public",
+    assetsDir: "public",
     devServerPort: 8085,
     // When using webpack-dev-server, you may need to redirect some calls
     // to a external API server. See https://webpack.js.org/configuration/dev-server/#devserver-proxy
     devServerProxy: undefined,
-    // Use babel-preset-env to generate JS compatible with most-used browsers.
-    // More info at https://babeljs.io/docs/en/next/babel-preset-env.html
-    babel: {
-        presets: [
-            // In case interop is used with React/Jsx components, this React preset would be required
-            ["@babel/preset-react"],
-            ["@babel/preset-env", {
-                "targets": "> 0.25%, not dead",
-                "modules": false,
-                // This adds polyfills when needed. Requires core-js dependency.
-                // See https://babeljs.io/docs/en/babel-preset-env#usebuiltins
-                "useBuiltIns": "usage",
-                "corejs": 3
-            }]
-        ],
-    }
 }
 
 // If we're running the webpack-dev-server, assume we're in development mode
@@ -79,13 +63,7 @@ module.exports = {
         // Split the code coming from npm packages into a different file.
         // 3rd party dependencies change less often, let the browser cache them.
         splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /node_modules/,
-                    name: "vendors",
-                    chunks: "all"
-                }
-            }
+            chunks: "all"
         },
     },
     plugins: commonPlugins.concat([
@@ -95,39 +73,24 @@ module.exports = {
             ]
         }),
     ]),
-
-    resolve: {
-        // See https://github.com/fable-compiler/Fable/issues/1490
-        symlinks: false,
-        modules: [resolve("./node_modules")],
-        alias: {
-            // Some old libraries still use an old specific version of core-js
-            // Redirect the imports of these libraries to the newer core-js
-            'core-js/es6': 'core-js/es'
-        }
-    },
     // Configuration for webpack-dev-server
     devServer: {
-        publicPath: "/",
-        contentBase: resolve(CONFIG.assetsDir),
+        static: {
+            publicPath: "/",
+            directory: resolve(CONFIG.assetsDir)
+        },
         port: CONFIG.devServerPort,
         proxy: CONFIG.devServerProxy,
-        hot: true,
-        inline: true
+        hot: true
     },
-    // - fable-loader: transforms F# into JS
-    // - babel-loader: transforms JS to old syntax (compatible with old browsers)
     // - sass-loaders: transforms SASS/SCSS into JS
-    // - file-loader: Moves files referenced in the code (fonts, images) into output folder
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(js)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: CONFIG.babel
-                }
+                enforce: "pre",
+                use: ["source-map-loader"]
             },
             {
                 test: /\.(sass|scss|css)$/,
@@ -146,7 +109,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*)?$/,
-                use: ["file-loader"]
+                type: 'assets/resource'
             }
         ]
     }
